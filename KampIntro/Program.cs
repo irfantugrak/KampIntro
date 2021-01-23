1d8fb93f155d87372ce6da8f5cdc5678df4248a3
İrfan TUĞRAK <rfan TURAK@DESKTOP-JFVLS0K>
2021-01-23 23:37:05 +0300

    İlk versiyon Add .gitignore and .gitattributes.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // type safety - tip güvenliği
+            // Do not repeat yourself - Kendini tekrarlama
+            // kategoriEtiketi - Değer tutucu & ALIAS
+
+            string kategoriEtiketi = "Kategoriler";
+            int ogren ciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.45;
+
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artıs butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+            
+            if (sistemeGirisYapmisMi == true)
+            { 
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giris yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
